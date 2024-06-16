c5b3112c3166fe48038750675c1b67aebbf586a3
priyanshusinha11 <priyanshusinha.2807@gmail.com>
2024-06-16 20:45:38 +0530

    controller setup

--- src/app.js
+++ src/app.js
@@ -9,11 +9,12 @@ app.use(cors({
 }))
 
 app.use(express.json({ limit: "16kb" }))
-
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-
 app.use(express.static("public"))
-
 app.use(cookieParser())
+
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users", userRouter)
 
 export { app }
