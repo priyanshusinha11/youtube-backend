ffd2a65fdfc8dd3e01a6a660dfc185b9c448dcd2
priyanshusinha11 <priyanshusinha.2807@gmail.com>
2024-06-23 22:30:05 +0530

    refactored logout function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -111,27 +111,33 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 
-const logoutUser = asyncHandler(async (req,res)=>{
-    await User.findByIdAndUpdate(
-        req.user._id,
-        {
-            $set: {
-                refreshToken: undefined
+const logoutUser = asyncHandler(async (req, res) => {
+    try {
+        await User.findByIdAndUpdate(
+            req.user._id,
+            {
+                $set: {
+                    refreshToken: undefined
+                }
+            },
+            {
+                new: true
             }
-        },
-        {
-            new: true
-        }
-    )
+        );
 
-    const options = {
-        httpOnly: true,
-        secure: true
+        const options = {
+            httpOnly: true,
+            secure: true
+        };
+
+        return res
+            .status(200)
+            .clearCookie("accessToken", options)
+            .clearCookie("refreshToken", options)
+            .json(new ApiResponse(200, null, "User logged out successfully"));
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Logout failed");
     }
-    return res
-    .status(200)
-    .clearCookie("accessToken", options)
-    .clearCookie("refreshToken", options)
-})
+});
 
 export { registerUser, loginUser, logoutUser };
