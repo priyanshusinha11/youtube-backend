9fccd05f41fb5cee23552ce0fb6ca1a5317c3f0d
priyanshusinha11 <priyanshusinha.2807@gmail.com>
2024-07-18 17:29:20 +0530

    subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,5 +1,4 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import { User } from "../models/user.model.js"
 import { Subscription } from "../models/subscription.model.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
@@ -8,17 +7,60 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const { channelId } = req.params
-    // TODO: toggle subscription
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    const subscriptionCheck = await Subscription.findOne({
+        channel: channelId,
+        subscriber: req.user?._id
+    })
+
+
+    if (subscriptionCheck) {
+        await subscriptionCheck.deleteOne()
+        return res.status(200).json(new ApiResponse(200, {}, "Subscription Removed Successfully"))
+    }
+
+    const createSubscription = await Subscription.create({
+        channel: channelId,
+        subscriber: req.user?._id
+    })
+
+    return res.status(200)
+        .json(new ApiResponse(200, createSubscription, "Congratulation! You have Successfully Subscribed this channel"))
 })
 
-// controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const { channelId } = req.params
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel id")
+    }
+
+    const subscribers = await Subscription.find({ channel: channelId }).populate("subscriber", "fullName email username avatar coverImage");
+
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { subscribers }, "Subscribers are fetched successfully"))
 })
 
-// controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    if (!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Invalid subscriberId id")
+    }
+
+    const subscribedChannels = await Subscription
+        .find({ subscriber: subscriberId })
+        .populate("channel", "fullName email username avatar coverImage");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { subscribedChannels }, "Subscribers are fetched successfully"))
 })
 
 export {
